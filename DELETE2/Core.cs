1eaaef5860edf25854f6c7329068928203c02d94
ViperisPRO <Valer4ukpro583@gmail.com>
2020-12-22 21:14:55 +0300

    1234

---
+++ DELETE2/Core.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DELETE2
+{
+    class Core
+    {
+        public static vkonstantinovEntities DB = new vkonstantinovEntities();
+    }
+}
---
+++ DELETE2/MainWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace DELETE2
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private List<View_StroyMaterial> _MyStroyMaterial; // 
+        public List<View_StroyMaterial> MyStroyMaterial   /* MyStroyMaterial это вставляется в MainWindow 
+                                                           <DataGrid Grid.Column="1" ItemsSource="{Binding MyStroyMaterial}" AutoGenerateColumns="False">
+                                                            MyStroyMaterial это присвоение значиеня*/
+        {
+            get
+            {
+                return _MyStroyMaterial; // _ обязательно
+            }
+            set
+            {
+                _MyStroyMaterial = value;
+            }
+        }
+        public MainWindow()
+        {
+            InitializeComponent();
+            this.DataContext = this;
+            MyStroyMaterial = Core.DB.View_StroyMaterial.ToList(); // подключение к БД, StroyMaterial это таблица
+        }
+
+    }
+}
